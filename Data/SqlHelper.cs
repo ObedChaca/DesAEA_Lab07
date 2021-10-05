fe782b5f95e7d07def3ff9824e0c2a355912ae30
Obed Chaca <obed.chaca@tecsup.edu.pe>
2021-10-04 22:05:59 -0500

    Agregar archivos de proyecto.

---
+++ Data/SqlHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Data
+{
+    static class SqlHelper
+    {
+        public static string Connection { get; } = "Data Source=LAPTOP-OBED\\SQLEXPRESS;" + "Initial Catalog=Neptuno;Integrated Security=true";
+
+        public static Int32 ExecuteNonQuery(String connectionString, String commandText, CommandType commandType, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    cmd.CommandType = commandType;
+                    cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static Object ExecuteScalar(String connectionString, String commandText, CommandType commandType, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    cmd.CommandType = commandType;
+                    cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public static SqlDataReader ExecuteReader(String connectionString, String commandText, CommandType commandType, params SqlParameter[] parameters)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            using (SqlCommand cmd = new SqlCommand(commandText, conn))
+            {
+                cmd.CommandType = commandType;
+                cmd.Parameters.AddRange(parameters);
+
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                return reader;
+            }
+        }
+
+    }
+}
---
+++ Semana07/ViewModel/ListaCategoriaViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using Semana07.View;
+
+namespace Semana07.ViewModel
+{
+    public class ListaCategoriaViewModel : ViewModelBase
+    {
+
+        ObservableCollection<Categoria> _Categorias;
+        //Cambios
+        public ObservableCollection<Categoria> Categorias
+        {
+            get { return _Categorias; }
+            set
+            {
+                if (_Categorias != value)
+                {
+                    _Categorias = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand NuevoCommand { set; get; }
+        public ICommand ConsultarCommand { set; get; }
+
+        public ListaCategoriaViewModel()
+        {
+            Categorias = new Model.CategoriaModel().Categorias;
+
+            NuevoCommand = new RelayCommand<Window>(
+                param => Abrir()
+                );
+
+            ConsultarCommand = new RelayCommand<object>(
+                o => { Categorias = (new Model.CategoriaModel()).Categorias; }
+                );
+
+            void Abrir()
+            {
+                ManCategoria window = new ManCategoria();
+                window.ShowDialog();
+                //Categorias = (new Model.CategoriaModel()).Categorias;
+            }
+        }
+    }
+}
